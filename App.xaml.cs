15328bab381920e480aa7c0e00dd610443d03762
Yatra <y.venzi@outlook.com>
2026-05-28 16:13:25 -0300

    cria página de cadastro com acesso ao DB

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using DotNetEnv;
+
+namespace CRUD;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Env.Load("C://Users//Aluno//RiderProjects//CRUD//.env");
+        
+        base.OnStartup(e);
+    }
+}
