237e03d70e548025e4f74b52b7f69fd9d7a06342
Yatra <y.venzi@outlook.com>
2026-06-17 16:33:22 -0300

    modifica o numero de curtidas imediatamente

--- Feed.xaml.cs
+++ Feed.xaml.cs
@@ -93,12 +93,14 @@ public partial class Feed : Window
                 query = "DELETE FROM curtidas_postagens WHERE usuario_id = @usuario AND postagem_id = @postagem";
                 acao = "descurtir";
                 postagem.FoiCurtido = false;
-            }
+                postagem.Curtidas--;
+            }    
             else
             {
                 query = "INSERT INTO curtidas_postagens(usuario_id, postagem_id) VALUES (@usuario, @postagem)";
                 acao = "curtir";
                 postagem.FoiCurtido = true;
+                postagem.Curtidas++;
             }
             conexao.Close();
             comando.CommandText = query;
--- Modelos/Postagem.cs
+++ Modelos/Postagem.cs
@@ -5,13 +5,23 @@ namespace CRUD.Modelos;
 
 public class Postagem : INotifyPropertyChanged
 {
+    private bool _foiCurtido;
+    private int _curtidas;
+    
     public int Id { get; set; }
     public string Conteudo { get; set; }
-    public int Curtidas { get; set; }
+    public int Curtidas 
+    { 
+        get => _curtidas;
+        set
+        {
+            _curtidas = value;
+            NotifyPropertyChanged();
+        }
+    }
     public DateTime Postado_em { get; set; }
     public Usuario Usuario { get; set; }
     public event PropertyChangedEventHandler? PropertyChanged;
-    private bool _foiCurtido;
 
     public bool FoiCurtido                                         
     {
