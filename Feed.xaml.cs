a2e9a9ecc1f2802065640704181eb0c4bee52d63
Yatra <y.venzi@outlook.com>
2026-06-17 14:58:30 -0300

    resgata posts curtidos pelo usuario

--- Feed.xaml.cs
+++ Feed.xaml.cs
@@ -22,11 +22,12 @@ public partial class Feed : Window
         List<Postagem> listaPostagens = [];
 
         const string query =
-            "SELECT p.id, p.conteudo, p.curtidas, p.postado_em, u.nome, u.username FROM postagens p INNER JOIN usuarios u ON  p.usuario_id = u.id";
+            "SELECT p.id, p.conteudo, p.curtidas, p.postado_em, u.nome, u.username, IF(cp.usuario_id IS NOT NULL, TRUE, FALSE) AS curtido FROM postagens p INNER JOIN usuarios u ON p.usuario_id = u.id LEFT JOIN curtidas_postagens cp ON cp.postagem_id = p.id AND cp.usuario_id = @usuario_id ORDER BY p.postado_em DESC";
 
         using var conexao = new MySqlConnection(App.StringConexao);
 
         using var comando = new MySqlCommand(query, conexao);
+        comando.Parameters.AddWithValue("@usuario_id", _usuario.Id);
 
         // Criar um bloco try-catch
         try
@@ -52,6 +53,7 @@ public partial class Feed : Window
                     Conteudo = leitor.GetString("conteudo"),
                     Curtidas = leitor.GetInt32("curtidas"),
                     Postado_em = leitor.GetDateTime("postado_em"),
+                    FoiCurtido =  leitor.GetBoolean("curtido"),
                     Usuario = new Usuario
                     {
                         Nome = leitor.GetString("nome"),
