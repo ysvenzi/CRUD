fd985c47fea5e3abf8fe7e64912cd13d7dab02a7
Yatra <y.venzi@outlook.com>
2026-06-18 14:40:54 -0300

    melhora o fluxo de uso do projeto

--- Cadastro.xaml.cs
+++ Cadastro.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using CRUD.Modelos;
 using MySql.Data.MySqlClient;
 
 namespace CRUD;
@@ -8,6 +9,7 @@ public partial class Cadastro : Window
     public Cadastro()
     {
         InitializeComponent();
+        TxtNome.Focus();
     }
 
     private void BtnCadastrar_OnClick(object sender, RoutedEventArgs e)
@@ -23,7 +25,7 @@ public partial class Cadastro : Window
 
         using var conexao = new MySqlConnection(App.StringConexao);
         const string query =
-            "INSERT INTO usuarios(nome, username, email, senha) VALUES(@nome, @username, @email, @senha)";
+            "INSERT INTO usuarios(nome, username, email, senha) VALUES(@nome, @username, @email, @senha); SELECT LAST_INSERT_ID();";
 
         using var comando = new MySqlCommand(query, conexao);
         comando.Parameters.AddWithValue("@nome", TxtNome.Text);
@@ -34,17 +36,24 @@ public partial class Cadastro : Window
         try
         {
             conexao.Open();
-            var linhasAfetadas = comando.ExecuteNonQuery();
-            if (linhasAfetadas > 0) MessageBox.Show("Cadastro realizado!");
+            var idGerado = comando.ExecuteScalar();
+            if (idGerado is null) throw new Exception("Cadastro não realizado!");
+            new Feed(new Usuario
+            {
+                Nome = TxtNome.Text,
+                Email = TxtEmail.Text,
+                Username = TxtUsername.Text,
+                Id = Convert.ToInt32(idGerado)
+            }).Show();
+            Close();
         }
         catch (Exception exception)
         {
-            if (exception is MySqlException erroSql)
-                if (erroSql.Number == 1062)
-                {
-                    MessageBox.Show("O email ou username já foram utilizados");
-                    return;
-                }
+            if (exception is MySqlException { Number: 1062 })
+            {
+                MessageBox.Show("O email ou username já foram utilizados");
+                return;
+            }
 
             Console.WriteLine(exception);
         }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        TxtUsuario.Focus();
     }
 
     private void BtnLogin_OnClick(object sender, RoutedEventArgs e)
@@ -46,15 +47,16 @@ public partial class MainWindow : Window
 
             while (leitor.Read())
             {
-                var usuarioBanco = new Usuario();
-
-                usuarioBanco.Id = leitor.GetInt32(0);
-                usuarioBanco.Nome = leitor.GetString(1);
-                usuarioBanco.Email = leitor.GetString(2);
-                usuarioBanco.Senha = leitor.GetString(3);
-                usuarioBanco.Username = leitor.GetString(4);
+                var usuarioBanco = new Usuario
+                {
+                    Id = leitor.GetInt32("id"),
+                    Nome = leitor.GetString("nome"),
+                    Email = leitor.GetString("email"),
+                    Username = leitor.GetString("username")
+                };
 
                 new Feed(usuarioBanco).Show();
+                Close();
             }
         }
         catch (Exception exception)
@@ -66,8 +68,7 @@ public partial class MainWindow : Window
     private void BtnCadastro_OnClick(object sender, RoutedEventArgs e)
     {
         var janelaCadastro = new Cadastro();
-        Hide();
-        janelaCadastro.ShowDialog();
-        Show();
+        janelaCadastro.Show();
+        Close(); 
     }
 }
